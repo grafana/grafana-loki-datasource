974186d5e6065690071d4279ac5e5fae7a072c2f
Nathan Verzemnieks <njvrzm@gmail.com>
2026-04-15 17:07:13 +0200

    Different TZ in grafana jest config vs here led to test failures

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-// force timezone to UTC to allow tests to work regardless of local timezone
-process.env.TZ = 'UTC';
+// We set this specifically for 2 reasons.
+// 1. It makes sense for both CI tests and local tests to behave the same so issues are found earlier
+// 2. Any wrong timezone handling could be hidden if we use UTC/GMT local time (which would happen in CI).
+process.env.TZ = 'Pacific/Easter'; // UTC-06:00 or UTC-05:00 depending on daylight savings
 
 module.exports = {
   ...require('./.config/jest.config'),
