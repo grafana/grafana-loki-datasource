d0112925143faaf9fea0f46f3f59fbef71907fc5
Matias Chomicki <matias.chomicki@grafana.com>
2026-09-07 14:04:15 +0000

    Fix: drop query statistics from empty frames (#192)

--- pkg/loki/framing_test.go
+++ pkg/loki/framing_test.go
@@ -80,6 +80,52 @@ func TestSuccessResponse(t *testing.T) {
 	}
 }
 
+func TestEmptyMetricResponseWithStatsDropsQueryStatisticsFrame(t *testing.T) {
+	body := []byte(`{
+		"status": "success",
+		"data": {
+			"resultType": "matrix",
+			"result": [],
+			"stats": {
+				"summary": {
+					"execTime": 0.001,
+					"totalBytesProcessed": 0,
+					"totalLinesProcessed": 0
+				}
+			}
+		}
+	}`)
+	query := lokiQuery{Expr: "count_over_time({app=\"foo\"}[1m])", Step: time.Second, QueryType: QueryTypeRange, Direction: DirectionBackward, RefID: "A"}
+
+	dr, err := runQuery(context.Background(), makeMockedAPI(http.StatusOK, "application/json", body, nil), &query, ResponseOpts{}, backend.NewLoggerWith("logger", "test"))
+	require.NoError(t, err)
+	require.Empty(t, dr.Frames)
+}
+
+func TestEmptyLogsResponseWithStatsKeepsLogsFrame(t *testing.T) {
+	body := []byte(`{
+		"status": "success",
+		"data": {
+			"resultType": "streams",
+			"result": [],
+			"stats": {
+				"summary": {
+					"execTime": 0.001,
+					"totalBytesProcessed": 0,
+					"totalLinesProcessed": 0
+				}
+			}
+		}
+	}`)
+	query := lokiQuery{Expr: `{app="foo"}`, QueryType: QueryTypeRange, Direction: DirectionBackward, RefID: "A"}
+
+	dr, err := runQuery(context.Background(), makeMockedAPI(http.StatusOK, "application/json", body, nil), &query, ResponseOpts{}, backend.NewLoggerWith("logger", "test"))
+	require.NoError(t, err)
+	require.Len(t, dr.Frames, 1)
+	require.GreaterOrEqual(t, len(dr.Frames[0].Fields), 4)
+	require.NotEqual(t, "Query statistics", dr.Frames[0].Name)
+}
+
 func TestErrorResponse(t *testing.T) {
 	// NOTE: when there is an error-response, it comes with
 	// HTTP code 400, and the format seems to change between versions:
